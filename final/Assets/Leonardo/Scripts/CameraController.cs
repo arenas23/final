546a5659e3b59358cfc39e34ded845393667ab46
Hernán Leonardo Quiroga Beltrán <hleonardoquirogab@hotmail.com>
2024-04-15 17:35:59 -0500

    Movimiento básico player y cámara, fase inicial

---
+++ final/Assets/Leonardo/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 80f;
+    public Transform playerBody;
+    public GameObject player;
+    float xRotation = 0;
+    
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerBody = player.GetComponent<Transform>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+        
+    }
+}
