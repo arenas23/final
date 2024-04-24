8314c45c8621ac9a90c060f86f77c44578f00188
Hernán Leonardo Quiroga Beltrán <hleonardoquirogab@hotmail.com>
2024-04-24 13:12:01 -0500

    UI health Player
    Creación de UI health para el player y funcional

---
+++ final/Assets/Leonardo/Scripts/UIPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class UIPlayer : MonoBehaviour
+{
+    PlayerController player;
+    TextMeshProUGUI textHealth;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+        textHealth = GameObject.Find("TextHealth").GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textHealth.text = "" + player.Health;
+    }
+}
