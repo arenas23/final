53a837b7f317888aa4b7b8caa7e4d2e0795916ec
Felipe Castillo <fcp323@gmail.com>
2024-04-30 21:57:25 -0500

    Add locked door mechanic

--- final/Assets/Scripts/DoorController.cs
+++ final/Assets/Scripts/DoorController.cs
@@ -8,35 +8,68 @@ public class DoorController : MonoBehaviour
 {
     // Start is called before the first frame update
     private Animator animator;
-    public Image open1, open2;
-    public TextMeshProUGUI unlock1, unlock2;
+    [Header("Door")]
+    [SerializeField] private bool requireKey = false;
+    [SerializeField] private Image open1;
+    [SerializeField] private Image open2;
+    [SerializeField] private TextMeshProUGUI doorText1;
+    [SerializeField] private TextMeshProUGUI doorText2;
+    [SerializeField] private Image circleBackground1;
+    [SerializeField] private Image circleBackground2;
+    [SerializeField] private Image textBackground1;
+    [SerializeField] private Image textBackground2;
+
+
+
+    [Header("Images")]
+    [SerializeField] private Sprite blueCircleBackground;
+    [SerializeField] private Sprite blueTextBackground;
+    [SerializeField] private Sprite redCircleBackground;
+    [SerializeField] private Sprite redTextBackground;
+
+    private bool isLocked = false;
     private bool canOpen = false;
     private bool isOpen = false;
+    private string unlocked = "Unlocked";
+    private string locked = "Locked";
 
     public void Start()
     {
         animator = GetComponentInParent<Animator>();
+        verifyKey();
     }
 
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.E) && canOpen)
         {
-            // Debug.Log("entre");
-            animator.Play("abrir");
-            isOpen = true;
-            AudioManager.Instance.PlaySfx("door_open");
+            if (isLocked)
+            {
+                Debug.Log("Missing key");
+            }
+            else
+            {
+                // Debug.Log("entre");
+                animator.Play("abrir");
+                isOpen = true;
+                AudioManager.Instance.PlaySfx("door_open");
+            }
+
         }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        verifyKey();
+        if (other.CompareTag("Player") && isLocked == false)
         {
             open1.gameObject.SetActive(true);
-            unlock1.gameObject.SetActive(false);
             open2.gameObject.SetActive(true);
-            unlock2.gameObject.SetActive(false);
+
+            doorText1.gameObject.SetActive(false);
+            doorText2.gameObject.SetActive(false);
+
             canOpen = true;
         }
     }
@@ -46,14 +79,48 @@ public class DoorController : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             open1.gameObject.SetActive(false);
-            unlock1.gameObject.SetActive(true);
             open2.gameObject.SetActive(false);
-            unlock2.gameObject.SetActive(true);
+
+            doorText1.gameObject.SetActive(true);
+            doorText2.gameObject.SetActive(true);
+
             canOpen = false;
             if (isOpen)
             {
                 animator.Play("cerrar");
                 isOpen = false;
+            }
+        }
+    }
+
+    private void verifyKey()
+    {
+        if (GameManager.Instance)
+        {
+            if (GameManager.Instance.keyCard == 0 && requireKey) isLocked = true;
+            else isLocked = false;
+
+            if (isLocked && requireKey)
+            {
+                circleBackground1.sprite = redCircleBackground;
+                circleBackground2.sprite = redCircleBackground;
+
+                textBackground1.sprite = redTextBackground;
+                textBackground2.sprite = redTextBackground;
+
+                doorText1.text = locked;
+                doorText2.text = locked;
+            }
+            else
+            {
+                circleBackground1.sprite = blueCircleBackground;
+                circleBackground2.sprite = blueCircleBackground;
+
+                textBackground1.sprite = blueTextBackground;
+                textBackground2.sprite = blueTextBackground;
+
+                doorText1.text = unlocked;
+                doorText2.text = unlocked;
             }
         }
     }
